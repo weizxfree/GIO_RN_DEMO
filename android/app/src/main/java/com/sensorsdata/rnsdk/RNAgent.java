45c27c422297045e791d68dc5c8153601049b181
weizhangxiang <weizhangxiang@sensorsdata.cn>
2020-03-12 16:43:31 +0800

    调通了插件代码

---
+++ android/app/src/main/java/com/sensorsdata/rnsdk/RNAgent.java
@@ -0,0 +1,14 @@
+package com.sensorsdata.rnsdk;
+
+import android.util.Log;
+import android.view.View;
+
+public class RNAgent {
+
+    private static final String TAG = "RNAgent";
+
+
+    public static void addView(View view, int index) {
+        Log.i(TAG, "insert success");
+    }
+}
